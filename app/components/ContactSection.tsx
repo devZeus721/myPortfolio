8c0172e4bc3dd0a576ba8f2644a4f4cb654b361f
devZeus MAtthew.K <devzeus.mathew721@gmail.com>
2025-07-12 20:21:08 +0530

    New Commitment
    Done

--- app/components/ContactSection.tsx
+++ app/components/ContactSection.tsx
@@ -38,10 +38,11 @@ export default function ContactSection() {
 							Get in Touch
 						</a>
 						<a
-							href="\finalcopy 010625.pdf"
-							target="_blank"
-							className="px-6 py-2 border border-gray-600 text-gray-300 font-medium rounded hover:bg-gray-800 transition-colors inline-flex items-center justify-center gap-2"
-						>
+  href="/DION JOHNSON mainfinall080725.pdf"
+  target="_blank"
+  className="px-6 py-2 border border-gray-600 text-gray-300 font-medium rounded hover:bg-gray-800 transition-colors inline-flex items-center justify-center gap-2"
+>
+
 							<svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
 								<path
 									fillRule="evenodd"
