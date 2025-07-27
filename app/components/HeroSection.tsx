21712c5a1aa2a3e999642c88af39bc5857ee9aa7
devZeus MAtthew.K <devzeus.mathew721@gmail.com>
2025-07-28 02:07:24 +0530

    done comited

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -7,7 +7,7 @@ export default function HeroSection() {
 		<section className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-12 sm:py-20">
 			<div className="text-center">
 				<div className="mb-6 sm:mb-8">
-					<Image src="U:\GithubDRive\myPortfolio\public\codechefp3.png" alt="Profile Picture" width={120} height={120} className="rounded-full mx-auto border-4 border-gray-700 shadow-lg" />
+					<Image src="/codechefp3.png" alt="Profile Picture" width={120} height={120} className="rounded-full mx-auto border-4 border-gray-700 shadow-lg" />
 				</div>
 				<h1 className="text-3xl sm:text-4xl font-bold text-white mb-3 sm:mb-4">Dion J MAtthew KODIYANPLCKAL</h1>
 				<p className="text-lg sm:text-xl text-gray-300 mb-6 sm:mb-8"><b>INDEPENDENT DESIGNER & WEBSITE CREATOR</b></p>
